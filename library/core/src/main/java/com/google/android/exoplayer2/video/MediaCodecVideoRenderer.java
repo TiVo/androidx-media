065de6620c40e68eb8e96ff669fb5f0c0db0e312
Steve Mayhew <mayhew.steve@gmail.com>
2019-11-19 16:51:26 -0800

    Cleanup onFrameRenderListener handler...
    Even if the codec is null (presume it's handler was run down first, this happens in `MediaCodec.stop()`) make sure to run down our own Handler.

--- library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
+++ library/core/src/main/java/com/google/android/exoplayer2/video/MediaCodecVideoRenderer.java
@@ -458,7 +458,7 @@ public class MediaCodecVideoRenderer extends MediaCodecRenderer {
     clearReportedVideoSize();
     clearRenderedFirstFrame();
     frameReleaseTimeHelper.disable();
-    if (tunnelingOnFrameRenderedListener != null && getCodec() != null) {
+    if (tunnelingOnFrameRenderedListener != null) {
       tunnelingOnFrameRenderedListener.destroyHandler(getCodec());
     }
     tunnelingOnFrameRenderedListener = null;
