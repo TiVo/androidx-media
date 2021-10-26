c58e66e1dece41429cdbc009e667de44998a76e7
Shashikant <60922344+shashikantkonded-git@users.noreply.github.com>
2021-10-26 19:41:18 +0530

    Synchronise buffering allocation (#191)

--- library/tivo-ui/src/main/java/com/tivo/android/utils/ByteBufferPool.java
+++ library/tivo-ui/src/main/java/com/tivo/android/utils/ByteBufferPool.java
@@ -9,7 +9,7 @@ import java.nio.ByteBuffer;
  */
 public class ByteBufferPool {
   private static final int BUFFER_POOL_SIZE = 3;
-  private static ByteBuffer[] buffersArray = new ByteBuffer[BUFFER_POOL_SIZE];;
+  private static ByteBuffer[] buffersArray = new ByteBuffer[BUFFER_POOL_SIZE];
   private static int buffersArrayCount;
 
   //pool ByteBuffers
@@ -19,8 +19,8 @@ public class ByteBufferPool {
         // Use the last buffer
         return buffersArray[--buffersArrayCount];
       }
+      return ByteBuffer.allocateDirect(bufferSize);
     }
-    return ByteBuffer.allocateDirect(bufferSize);
   }
 
   public static void releaseBuffer(ByteBuffer buffer) {
