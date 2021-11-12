google/ExoPlayer

Ensure DefaultDrmSessions keep working if their manager is released
This change introduces a third 'state' for `DefaultDrmSessionManager`:
It's been fully released (prepareCount == 0) but at least one of its
sessions is still active.

In this state new acquisitions are rejected (`(pre)acquireSession()`
calls will fail) but the machinery to support the existing sessions
(ExoMediaDrm and MediaDrmHandler) is kept until they're all released.

This change will allow us to remove the TODO in MediaCodecRenderer
that resolves Issue: #8842.

PiperOrigin-RevId: 376193952

library/core/src/main/java/com/google/android/exoplayer2/drm/DefaultDrmSessionManager.java
@@ -457,9 +457,15 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
     if (prepareCallsCount++ != 0) {
       return;
     }
-    checkState(exoMediaDrm == null);
-    exoMediaDrm = exoMediaDrmProvider.acquireExoMediaDrm(uuid);
-    exoMediaDrm.setOnEventListener(new MediaDrmEventListener());
+    if (exoMediaDrm == null) {
+      exoMediaDrm = exoMediaDrmProvider.acquireExoMediaDrm(uuid);
+      exoMediaDrm.setOnEventListener(new MediaDrmEventListener());
+    } else if (sessionKeepaliveMs != C.TIME_UNSET) {
+      // Re-acquire the keepalive references for any sessions that are still active.
+      for (int i = 0; i < sessions.size(); i++) {
+        sessions.get(i).acquire(/* eventDispatcher= */ null);
+      }
+    }
   }
 
   @Override
@@ -478,8 +484,7 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
     }
     releaseAllPreacquiredSessions();
 
-    checkNotNull(exoMediaDrm).release();
-    exoMediaDrm = null;
+    maybeReleaseMediaDrm();
   }
 
   @Override
@@ -776,6 +781,17 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
     return session;
   }
 
+  private void maybeReleaseMediaDrm() {
+    if (exoMediaDrm != null
+        && prepareCallsCount == 0
+        && sessions.isEmpty()
+        && preacquiredSessionReferences.isEmpty()) {
+      // This manager and all its sessions are fully released so we can release exoMediaDrm.
+      checkNotNull(exoMediaDrm).release();
+      exoMediaDrm = null;
+    }
+  }
+
   /**
    * Extracts {@link SchemeData} instances suitable for the given DRM scheme {@link UUID}.
    *
@@ -897,6 +913,7 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
           keepaliveSessions.remove(session);
         }
       }
+      maybeReleaseMediaDrm();
     }
   }
 

TiVo/androidx-media

library/core/src/main/java/com/google/android/exoplayer2/drm/DefaultDrmSessionManager.java
@@ -446,9 +446,15 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
     if (prepareCallsCount++ != 0) {
       return;
     }
-    Assertions.checkState(exoMediaDrm == null);
-    exoMediaDrm = exoMediaDrmProvider.acquireExoMediaDrm(uuid);
-    exoMediaDrm.setOnEventListener(new MediaDrmEventListener());
+    if (exoMediaDrm == null) {
+      exoMediaDrm = exoMediaDrmProvider.acquireExoMediaDrm(uuid);
+      exoMediaDrm.setOnEventListener(new MediaDrmEventListener());
+    } else if (sessionKeepaliveMs != C.TIME_UNSET) {
+      // Re-acquire the keepalive references for any sessions that are still active.
+      for (int i = 0; i < sessions.size(); i++) {
+        sessions.get(i).acquire(/* eventDispatcher= */ null);
+      }
+    }
   }
 
   @Override
@@ -463,8 +469,7 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
       // Release all the keepalive acquisitions.
       sessions.get(i).release(/* eventDispatcher= */ null);
     }
-    Assertions.checkNotNull(exoMediaDrm).release();
-    exoMediaDrm = null;
+    maybeReleaseMediaDrm();
   }
 
   @Override
@@ -691,6 +696,17 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
     return session;
   }
 
+  private void maybeReleaseMediaDrm() {
+    if (exoMediaDrm != null
+        && prepareCallsCount == 0
+        && sessions.isEmpty()) {
+      // This manager and all its sessions are fully released so we can release exoMediaDrm.
+      Assertions.checkNotNull(exoMediaDrm);
+      exoMediaDrm.release();
+      exoMediaDrm = null;
+    }
+  }
+
   /**
    * Extracts {@link SchemeData} instances suitable for the given DRM scheme {@link UUID}.
    *
@@ -812,6 +828,7 @@ public class DefaultDrmSessionManager implements DrmSessionManager {
           keepaliveSessions.remove(session);
         }
       }
+      maybeReleaseMediaDrm();
     }
   }
 