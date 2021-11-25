f28b802205c7c40147cf2bdc3948d83ef77c3667
Shashikant <60922344+shashikantkonded-git@users.noreply.github.com>
2021-11-25 11:11:24 +0530

    Double null check init is added

--- library/tivo-ui/src/main/java/com/tivo/android/utils/ByteBufferPool.java
+++ library/tivo-ui/src/main/java/com/tivo/android/utils/ByteBufferPool.java
@@ -10,34 +10,35 @@ import java.nio.ByteBuffer;
 public class ByteBufferPool {
   private static final int BUFFER_POOL_SIZE = 3;
 
-  private static ByteBufferPool gByteBufferPool;
-  private ByteBuffer[] buffersArray;
+  private static volatile ByteBufferPool gByteBufferPool;
+  private final ByteBuffer[] buffersArray;
   private int buffersArrayCount;
 
   private ByteBufferPool(){
     buffersArray =new ByteBuffer[BUFFER_POOL_SIZE];
   }
 
-  public static synchronized ByteBufferPool getInstance(){
-    if(gByteBufferPool ==null){
-      gByteBufferPool = new ByteBufferPool();
+  public static ByteBufferPool getInstance() {
+    if (gByteBufferPool == null) {
+      synchronized (ByteBufferPool.class) {
+        if (gByteBufferPool == null) {
+          gByteBufferPool = new ByteBufferPool();
+        }
+      }
     }
     return gByteBufferPool;
   }
 
   //pool ByteBuffers
-  public ByteBuffer acquireBuffer(int bufferSize) {
-    synchronized (buffersArray) {
+  public synchronized ByteBuffer acquireBuffer(int bufferSize) {
       if (buffersArrayCount > 0) {
         // Use the last buffer
         return buffersArray[--buffersArrayCount];
       }
       return ByteBuffer.allocateDirect(bufferSize);
-    }
   }
 
-  public void releaseBuffer(ByteBuffer buffer) {
-    synchronized (buffersArray) {
+  public synchronized void releaseBuffer(ByteBuffer buffer) {
       // If the pool is not at capacity yet, then add this one to the
       // pool
       if (buffersArrayCount < BUFFER_POOL_SIZE) {
@@ -45,6 +46,5 @@ public class ByteBufferPool {
       }
       // Otherwise, just drop this buffer and let it get garbage
       // collected
-    }
   }
 }
