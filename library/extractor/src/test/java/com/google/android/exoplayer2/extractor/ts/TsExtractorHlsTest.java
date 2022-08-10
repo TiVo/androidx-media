0f47806ffd352b5f876b3553f47929159521b68b
Zachary Mayhew <zachary.mayhew@xperi.com>
2022-08-10 13:03:15 -0700

    add unit tests for iframe transport segments with filler data

--- library/extractor/src/test/java/com/google/android/exoplayer2/extractor/ts/TsExtractorHlsTest.java
+++ library/extractor/src/test/java/com/google/android/exoplayer2/extractor/ts/TsExtractorHlsTest.java
@@ -132,4 +132,57 @@ public class TsExtractorHlsTest {
     assertThat(sampleData.length).isEqualTo(245324);
     assertThat(Arrays.hashCode(sampleData)).isEqualTo(0x569E5F74);
   }
+
+  @Test
+  public void consumeIFrameWithFillerData() throws Exception {
+    TsPayloadReader.Factory factory = new DefaultTsPayloadReaderFactory();
+    final int firstSampleTimestampUs = 123;
+    TsExtractor tsExtractor =
+            new TsExtractor(TsExtractor.MODE_HLS, new TimestampAdjuster(firstSampleTimestampUs), factory);
+    FakeExtractorOutput output = new FakeExtractorOutput();
+    tsExtractor.init(output);
+
+    FakeExtractorInput initInput =
+            new FakeExtractorInput.Builder()
+                    .setData(
+                            TestUtil.getByteArray(
+                                    ApplicationProvider.getApplicationContext(),
+                                    "media/ts/sample_h264_filler_ending.ts"))
+                    .setSimulateIOErrors(false)
+                    .setSimulateUnknownLength(false)
+                    .setSimulatePartialReads(false)
+                    .build();
+
+    PositionHolder seekPositionHolder = new PositionHolder();
+
+    int readResult = Extractor.RESULT_CONTINUE;
+    while (readResult != Extractor.RESULT_END_OF_INPUT) {
+      readResult = tsExtractor.read(initInput, seekPositionHolder);
+      if (readResult == Extractor.RESULT_SEEK) {
+        initInput.setPosition((int) seekPositionHolder.position);
+      }
+    }
+
+    assertThat(output.numberOfTracks).isEqualTo(2);
+    FakeTrackOutput trackOutput = output.trackOutputs.get(27);
+    assertThat(trackOutput).isNotNull();
+
+    Format actualFormat = trackOutput.lastFormat;
+    Format expectedFormat = new Format.Builder()
+            .setId("1/27")
+            .setSampleMimeType("video/avc")
+            .setCodecs("avc1.4D401E")
+            .setWidth(640)
+            .setHeight(360)
+            .setInitializationData(actualFormat.initializationData)   // Ignore this.
+            .build();
+    assertThat(actualFormat).isEqualTo(expectedFormat);
+
+    // One sample, with the Access Unit containing the IDR and associated NALU is present
+    trackOutput.assertSampleCount(1);
+    assertThat(trackOutput.getSampleTimeUs(0)).isEqualTo(firstSampleTimestampUs);
+    byte[] sampleData = trackOutput.getSampleData(0);
+    assertThat(sampleData.length).isEqualTo(46777);
+    assertThat(Arrays.hashCode(sampleData)).isEqualTo(0xa3c73157);
+  }
 }
