d4bdb3c61739471534922710cd6ab65fcc320967
Zachary Mayhew <zachary.mayhew@xperi.com>
2022-08-12 13:47:39 -0700

    add javadocs and fix up StressBasedTrackSelector

--- library/tivo-ui/src/main/java/com/tivo/exoplayer/library/tracks/StressBasedTrackSelector.java
+++ library/tivo-ui/src/main/java/com/tivo/exoplayer/library/tracks/StressBasedTrackSelector.java
@@ -19,40 +19,73 @@ import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A custom track selector that limits frame rate to various degrees depending on the provided
+ * {@link PlayerStressLevel}
+ */
 public class StressBasedTrackSelector extends DefaultTrackSelector {
 
+  /**
+   * The stress level of the player. One of {@link StressBasedTrackSelector#PLAYER_STRESS_NONE},
+   * {@link StressBasedTrackSelector#PLAYER_STRESS_MODERATE},
+   * {@link StressBasedTrackSelector#PLAYER_STRESS_SEVERE}, or
+   * {@link StressBasedTrackSelector#PLAYER_STRESS_CRITICAL}.
+   */
   @Retention(RetentionPolicy.SOURCE)
   @IntDef({PLAYER_STRESS_NONE, PLAYER_STRESS_MODERATE, PLAYER_STRESS_SEVERE, PLAYER_STRESS_CRITICAL})
   public @interface PlayerStressLevel {}
+  /**
+   * Does not limit frame rate or resolution
+   */
   public static final int PLAYER_STRESS_NONE = 0;
+  /**
+   * Limits 60fps content above 2k
+   */
   public static final int PLAYER_STRESS_MODERATE = 1;
+  /**
+   * Limits 60fps content above 1080p
+   */
   public static final int PLAYER_STRESS_SEVERE = 2;
+  /**
+   * Limits all content above 30fps
+   */
   public static final int PLAYER_STRESS_CRITICAL = 3;
 
   @PlayerStressLevel private int currentStressLevel = PLAYER_STRESS_NONE;
 
+  /**
+   * @see DefaultTrackSelector#DefaultTrackSelector(Context) 
+   */
   public StressBasedTrackSelector(Context context) {
     super(context);
   }
 
+  /**
+   * @see DefaultTrackSelector#DefaultTrackSelector(Context, TrackSelection.Factory)
+   */
   public StressBasedTrackSelector(Context context, TrackSelection.Factory trackSelectionFactory) {
     super(context, trackSelectionFactory);
   }
 
+  /**
+   * @see DefaultTrackSelector#DefaultTrackSelector(Parameters, TrackSelection.Factory)
+   */
   public StressBasedTrackSelector(Parameters parameters, TrackSelection.Factory trackSelectionFactory) {
     super(parameters, trackSelectionFactory);
   }
 
   /**
    * Gets the current stress level
+   * @see PlayerStressLevel
    * @return the current stress level
    */
-  @PlayerStressLevel int getCurrentStressLevel() {
+  public @PlayerStressLevel int getCurrentStressLevel() {
     return currentStressLevel;
   }
 
   /**
    * Sets the current stress level
+   * @see PlayerStressLevel
    * @param currentStressLevel the current stress level
    */
   public void setCurrentStressLevel(@PlayerStressLevel int currentStressLevel) {
@@ -84,6 +117,7 @@ public class StressBasedTrackSelector extends DefaultTrackSelector {
 
     switch (currentStressLevel) {
       case PLAYER_STRESS_NONE:
+        // Do not filter content
         return definition;
       case PLAYER_STRESS_MODERATE:
         // Filter Content over 2k @ 60fps (allows 4k@30fps)
@@ -119,7 +153,8 @@ public class StressBasedTrackSelector extends DefaultTrackSelector {
       return definition;
     }
 
-    return new TrackSelection.Definition(definition.group, Ints.toArray(filteredTracks), definition.reason, definition.data);
+    return new TrackSelection.Definition(
+            definition.group, Ints.toArray(filteredTracks), definition.reason, definition.data);
   }
 
 }
