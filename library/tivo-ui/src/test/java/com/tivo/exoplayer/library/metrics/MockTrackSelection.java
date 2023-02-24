cccb703415569e2c2d811935a0972a567a6ea294
Steve Mayhew <mayhew.steve@gmail.com>
2023-02-24 10:58:29 -0800

    The `testGetTimeInFormat_AllwaysPlayingState()` test passes
    Modified the test to call `AnalyticsListener.onEvent()` paired with each series of
    other AnalyticsListener calls with the same EventTime.
    
    Added a player mock and test track selector to use to pass to onEvent()

---
+++ library/tivo-ui/src/test/java/com/tivo/exoplayer/library/metrics/MockTrackSelection.java
@@ -0,0 +1,59 @@
+package com.tivo.exoplayer.library.metrics;
+
+import androidx.annotation.Nullable;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.source.TrackGroup;
+import com.google.android.exoplayer2.source.chunk.MediaChunk;
+import com.google.android.exoplayer2.source.chunk.MediaChunkIterator;
+import com.google.android.exoplayer2.trackselection.BaseTrackSelection;
+import java.util.List;
+
+/**
+ * Use for PlaybackStatsListener test cases so the player mock can
+ * return a track selection with a fixed track selected.
+ */
+class MockTrackSelection extends BaseTrackSelection {
+
+  private int selectedIndex;
+
+  public static MockTrackSelection buildFrom(Format[] formats) {
+    TrackGroup group = new TrackGroup(formats);
+    int[] tracks = new int[formats.length];
+    for (int i = 0; i < tracks.length; i++) {
+      tracks[i] = i;
+    }
+    return new MockTrackSelection(group, tracks);
+  }
+
+  public MockTrackSelection(TrackGroup group, int... tracks) {
+    super(group, tracks);
+    selectedIndex = 0;
+  }
+
+  public void setSelectedIndex(int index) {
+    selectedIndex = index;
+  }
+
+  @Override
+  public int getSelectedIndex() {
+    return selectedIndex;
+  }
+
+  @Override
+  public int getSelectionReason() {
+    return 0;
+  }
+
+  @Nullable
+  @Override
+  public Object getSelectionData() {
+    return null;
+  }
+
+  @Override
+  public void updateSelectedTrack(long playbackPositionUs, long bufferedDurationUs,
+      long availableDurationUs, List<? extends MediaChunk> queue,
+      MediaChunkIterator[] mediaChunkIterators) {
+
+  }
+}
--- library/tivo-ui/src/test/java/com/tivo/exoplayer/library/metrics/PlaybackStatsExtensionTest.java
+++ library/tivo-ui/src/test/java/com/tivo/exoplayer/library/metrics/PlaybackStatsExtensionTest.java
@@ -1,16 +1,26 @@
 package com.tivo.exoplayer.library.metrics;
 
+import android.util.SparseArray;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.util.FlagSet;
 import java.util.Map;
 
 import com.google.android.exoplayer2.source.MediaLoadData;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.google.android.exoplayer2.analytics.AnalyticsListener.EVENT_DOWNSTREAM_FORMAT_CHANGED;
+import static com.google.android.exoplayer2.analytics.AnalyticsListener.EVENT_PLAYBACK_STATE_CHANGED;
+import static com.google.android.exoplayer2.analytics.AnalyticsListener.EVENT_PLAY_WHEN_READY_CHANGED;
+import static com.google.android.exoplayer2.analytics.AnalyticsListener.EVENT_TIMELINE_CHANGED;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
+import static org.mockito.Mockito.when;
 
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.Format;
@@ -21,38 +31,70 @@ import com.google.android.exoplayer2.analytics.PlaybackStats;
 import com.google.android.exoplayer2.analytics.PlaybackStatsListener;
 import com.google.android.exoplayer2.testutil.FakeTimeline;
 import com.google.android.exoplayer2.util.MimeTypes;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@Ignore("need another approach then calling AnalyticsListner methods")
 @RunWith(AndroidJUnit4.class)
 public class PlaybackStatsExtensionTest {
 
-    static final Format TEST_BASEVIDEO_FORMAT = Format.createVideoSampleFormat(null,
-            MimeTypes.VIDEO_H264, null, Format.NO_VALUE, Format.NO_VALUE, 1280, 720, Format.NO_VALUE,
-            null, null);
-    static final Format TEST_BASEAUDIO_FORMAT = Format.createAudioSampleFormat(null,
-            MimeTypes.AUDIO_AAC,null,Format.NO_VALUE,Format.NO_VALUE,Format.NO_VALUE,Format.NO_VALUE,
-            null,null,Format.NO_VALUE,null);
+    static final Format TEST_BASEVIDEO_FORMAT = new Format.Builder()
+        .setSampleMimeType(MimeTypes.VIDEO_H264)
+        .setWidth(1280)
+        .setHeight(720)
+        .build();
+    static final Format TEST_BASEAUDIO_FORMAT = new Format.Builder()
+        .setSampleMimeType(MimeTypes.AUDIO_AAC)
+        .build();
     static final Timeline TEST_TIMELINE = new FakeTimeline(/* windowCount= */ 1);
+
+    @Mock
+    Player playerMock;
+
+    @Before
+    public void setupMock() {
+        MockitoAnnotations.openMocks(this);
+
+        // Calls to the player from PlaybackStatsListener that are invariant
+        when(playerMock.getPlaybackParameters()).thenReturn(PlaybackParameters.DEFAULT);
+        when(playerMock.getPlayWhenReady()).thenReturn(false);
+    }
 
     @Test
     public void testGetTimeInFormat_AllwaysPlayingState() {
-        Format formats[] = {
+        Format[] formats = {
                 TEST_BASEVIDEO_FORMAT.buildUpon().setAverageBitrate(10).setPeakBitrate(10).build(), TEST_BASEVIDEO_FORMAT.buildUpon().setAverageBitrate(20).setPeakBitrate(20).build(), TEST_BASEVIDEO_FORMAT.buildUpon().setAverageBitrate(30).setPeakBitrate(30).build(),
         };
 
+        MockTrackSelection trackSelection = MockTrackSelection.buildFrom(formats);
+        TrackSelectionArray trackSelectionArray = new TrackSelectionArray(trackSelection);
+        when(playerMock.getCurrentTrackSelections()).thenReturn(trackSelectionArray);
+
         PlaybackStatsListener playbackStatsListener = new PlaybackStatsListener(/* keepHistory= */ true, /* callback= */ null);
-        playbackStatsListener.onTimelineChanged(createEventTime(0), Player.TIMELINE_CHANGE_REASON_SOURCE_UPDATE);
 
-        playbackStatsListener.onDownstreamFormatChanged(createEventTime(0), createMediaLoad(formats[0]));
-        playbackStatsListener.onPlaybackStateChanged(createEventTime(5), Player.STATE_READY);
-        playbackStatsListener.onPlayWhenReadyChanged(createEventTime(5), true, Player.PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST);
+        AnalyticsListener.EventTime eventTime = createEventTime(0);
+        playbackStatsListener.onTimelineChanged(eventTime, Player.TIMELINE_CHANGE_REASON_SOURCE_UPDATE);
+        playbackStatsListener.onDownstreamFormatChanged(eventTime, createMediaLoad(formats[0]));
+        when(playerMock.getPlayerError()).thenReturn(null);
+        playbackStatsListener.onEvents(playerMock, createEventsAtSameEventTime(eventTime, EVENT_TIMELINE_CHANGED, EVENT_DOWNSTREAM_FORMAT_CHANGED));
 
-        playbackStatsListener.onDownstreamFormatChanged(createEventTime(30), createMediaLoad(formats[1]));
-        playbackStatsListener.onDownstreamFormatChanged(createEventTime(100), createMediaLoad(formats[2]));
-        playbackStatsListener.onDownstreamFormatChanged(createEventTime(160), createMediaLoad(formats[0]));
-        playbackStatsListener.onDownstreamFormatChanged(createEventTime(200), createMediaLoad(formats[1]));
-        playbackStatsListener.onPlaybackStateChanged(createEventTime(300), Player.STATE_ENDED);
-        playbackStatsListener.onPlayWhenReadyChanged(createEventTime(300), false, Player.PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST);
+        when(playerMock.getPlayWhenReady()).thenReturn(true);
+        when(playerMock.getPlaybackState()).thenReturn(Player.STATE_READY);
+        eventTime = createEventTime(5);
+        playbackStatsListener.onPlaybackStateChanged(eventTime, Player.STATE_READY);
+        playbackStatsListener.onPlayWhenReadyChanged(eventTime, true, Player.PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST);
+        playbackStatsListener.onEvents(playerMock, createEventsAtSameEventTime(eventTime, EVENT_PLAY_WHEN_READY_CHANGED, EVENT_PLAYBACK_STATE_CHANGED));
+
+        trackSelection.setSelectedIndex(1); postDownStreamFormatChange(playbackStatsListener, formats[1], createEventTime(30));
+        trackSelection.setSelectedIndex(2); postDownStreamFormatChange(playbackStatsListener, formats[2], createEventTime(100));
+        trackSelection.setSelectedIndex(0); postDownStreamFormatChange(playbackStatsListener, formats[0], createEventTime(160));
+        trackSelection.setSelectedIndex(1); postDownStreamFormatChange(playbackStatsListener, formats[1], createEventTime(200));
+
+        when(playerMock.getPlayWhenReady()).thenReturn(false);
+        when(playerMock.getPlaybackState()).thenReturn(Player.STATE_ENDED);
+        eventTime = createEventTime(300);
+        playbackStatsListener.onPlaybackStateChanged(eventTime, Player.STATE_ENDED);
+        playbackStatsListener.onPlayWhenReadyChanged(eventTime, false, Player.PLAY_WHEN_READY_CHANGE_REASON_USER_REQUEST);
+        playbackStatsListener.onEvents(playerMock, createEventsAtSameEventTime(eventTime, EVENT_PLAY_WHEN_READY_CHANGED, EVENT_PLAYBACK_STATE_CHANGED));
 
         long[] expected = {
                 (30 - 5) + (200 - 160),
@@ -77,6 +119,7 @@ public class PlaybackStatsExtensionTest {
         assertThat(totalInFormats).isEqualTo(stats.getTotalPlayTimeMs());
     }
 
+    @Ignore
     @Test
     public void testGetTimeInFormat_PlayingStateChange() {
         Format formats[] = {
@@ -123,6 +166,7 @@ public class PlaybackStatsExtensionTest {
 
     }
 
+    @Ignore
     @Test
     public void testGetTimeInFormat_PlayingStateChangeWithLastStatePlaying() {
         Format formats[] = {
@@ -163,6 +207,22 @@ public class PlaybackStatsExtensionTest {
 
         assertThat(totalInFormats).isEqualTo(stats.getTotalPlayTimeMs());
 
+    }
+
+
+    void postDownStreamFormatChange(AnalyticsListener listener, Format format, AnalyticsListener.EventTime eventTime) {
+        AnalyticsListener.Events events = createEventsAtSameEventTime(eventTime, EVENT_DOWNSTREAM_FORMAT_CHANGED);
+        listener.onDownstreamFormatChanged(eventTime, createMediaLoad(format));
+        listener.onEvents(playerMock, events);
+    }
+
+
+    static AnalyticsListener.Events createEventsAtSameEventTime(AnalyticsListener.EventTime eventTime, int... flags) {
+        SparseArray<AnalyticsListener.EventTime> eventTimes = new SparseArray<>();
+        for (int eventFlag : flags) {
+            eventTimes.put(eventFlag, eventTime);
+        }
+        return new AnalyticsListener.Events(new FlagSet.Builder().addAll(flags).build(), eventTimes);
     }
 
     static MediaLoadData createMediaLoad(Format format) {
